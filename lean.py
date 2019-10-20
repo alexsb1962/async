0bad5134b318ecdf2739a92da76b3fe533871c83
alexsb1962 <alexsb@mail.ru>
2019-10-20 23:52:52 +0300

    возможены разные способы сравнения при формировании следовательности.Реализация будет зависеть от конкретных условий применения.Итератор общего вида не актуален?

--- lean.py
+++ lean.py
@@ -4,18 +4,19 @@ import time
 #import sys
 
 class frange():
-    def __init__(self,start:float,end:float,dx:float=1.0):
+    def __init__(self,start:float,end:float,dx:float=1.0,eps:float=sys.float_info.epsilon):
         if dx == 0.0:
             raise StopIteration
-        if dx < 0:
-            self.xe = min(start,end)
-            self.xs = max(start,end)
         else:
-            self.xs = min(start,end)
-            self.xe = max(start,end)
+            if dx < 0:
+                self.xe = min(start,end)
+                self.xs = max(start,end)
+            else:
+                self.xs = min(start,end)
+                self.xe = max(start,end)
 
-        self.xd =dx
-        self.x=self.xs
+            self.xd =dx
+            self.x=self.xs
 
     def __iter__(self):
         return(self)
@@ -24,25 +25,21 @@ class frange():
         result=self.x
         self.x+=self.xd
         if self.xd>0:
-            if result>=self.xe:
+            if result>self.xe:
                 raise StopIteration
         else:
-            if result<=self.xe:
+            if result<self.xe:
                 raise StopIteration
         return(result)
 
 
 
-def fun1():
-    pass
-
-def fun2():
-    pass
-
-
 def main():
-    for a in frange(-5.0,-1.0,0.5):
-        print(a)
+    l=[]
+    print(list(l for l in frange(1.0,5.0,0.5)))
+    print(list(l for l in frange(5.0,1.0,0.5)))
+    print(list(l for l in frange(-1.0,-5.0,0.5)))
+    print(list(l for l in frange(-5.0,-1.0,-0.5)))
 
 
 if __name__=="__main__":
