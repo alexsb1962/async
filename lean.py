92f9c028a0c915ab661834ad3e47b89055993c10
alexsb1962 <alexsb@mail.ru>
2019-10-20 10:40:07 +0300

    first

---
+++ lean.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+
+import asyncio
+import time
+#import sys
+
+
+
+async def fun1():
+    pass
+
+async def fun2():
+    pass
+
+
+async def main():
+    pass
+
+
+if __name__=='__main__':
+    main()
